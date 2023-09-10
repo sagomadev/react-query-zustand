ae8c042d9df8e65a31d67226f78c5981ea8cde86
Samuel Gómez <sagomadev@gmail.com>
2023-09-10 21:21:17 +0200

    Refactor useRepos Hook

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -1,13 +1,15 @@
-import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import api from "../api/github";
 import { Repository } from "./types";
 
-async function fetchRepos(ctx: QueryFunctionContext) {
-  const [, githubUser] = ctx.queryKey;
+async function fetchRepos(githubUser: string) {
   const { data } = await api.get<Repository[]>(`/users/${githubUser}/repos`);
   return data;
 }
 
 export function useFetchRepositories(user: string) {
-  return useQuery(["repos", user], fetchRepos);
+  return useQuery({
+    queryKey: ["repos", user],
+    queryFn: () => fetchRepos(user),
+  });
 }
